5cd764718a8237f1d06a275dc60ffaeedc16e0b8
Bart Massey <bart.massey@gmail.com>
2021-04-15 22:50:11 -0700

    implemented into_iter

--- mutlist.rs
+++ mutlist.rs
@@ -1,3 +1,5 @@
+// https://rust-unofficial.github.io/too-many-lists/second.html
+
 struct Node<T> {
     data: T,
     next: Option<Box<Node<T>>>,
@@ -5,9 +7,26 @@ struct Node<T> {
 
 pub struct List<T>(Option<Box<Node<T>>>);
 
-// https://rust-unofficial.github.io/too-many-lists/second-iter.html
+struct IntoIter<T> {
+    cur: Option<Box<Node<T>>>,
+}
+
 struct Iter<'a, T> {
     cur: Option<&'a Node<T>>,
+}
+
+struct IterMut<'a, T> {
+    cur: Option<&'a mut Node<T>>,
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        let head = self.cur.take()?;
+        let data = head.data;
+        *self = IntoIter::new(head.next);
+        Some(data)
+    }
 }
 
 impl<'a, T> Iterator for Iter<'a, T> {
@@ -20,9 +39,31 @@ impl<'a, T> Iterator for Iter<'a, T> {
     }
 }
 
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.cur.take().map(|node| {
+            self.cur = node.next.as_deref_mut();
+            &mut node.data
+        })
+    }
+}
+
+impl<T> IntoIter<T> {
+    fn new(cur: Option<Box<Node<T>>>) -> Self {
+        IntoIter { cur }
+    }
+}
+
 impl<'a, T> Iter<'a, T> {
     fn new(cur: Option<&'a Node<T>>) -> Self {
         Iter { cur }
+    }
+}
+
+impl<'a, T> IterMut<'a, T> {
+    fn new(cur: Option<&'a mut Node<T>>) -> Self {
+        IterMut { cur }
     }
 }
 
@@ -43,7 +84,15 @@ impl<T> List<T> {
         Some(data)
     }
 
+    pub fn into_iter(self) -> impl Iterator<Item = T> {
+        IntoIter::new(self.0)
+    }
+
     pub fn iter(&self) -> impl Iterator<Item = &T> {
         Iter::new(self.0.as_deref())
+    }
+
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut T> {
+        IterMut::new(self.0.as_deref_mut())
     }
 }
